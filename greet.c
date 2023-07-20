843b6b1e8dd415d1dbfa40425c063b1f3b137d99
Just-Code-X <134972821+Just-Code-X@users.noreply.github.com>
2023-07-21 03:46:54 +0800

    Add files via upload

---
+++ greet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+float c, fh;
+printf("Enter temperature in Fahrenhiet\n ");
+scanf("%f" ,fh);
+c = (fh-32)/1.8;
+printf("Your temperature in ceLsius is ,%.1f\n",c);
+
+
+
+
+}
